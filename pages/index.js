cf3bc687ffe646d5c4f18af44a06569e14a7b528
daroGH <chandaro37@gmail.com>
2021-10-05 11:07:20 +1030

    another rework try to access dynamically value

--- pages/index.js
+++ pages/index.js
@@ -1,107 +1,125 @@
 import { useState, useEffect } from "react";
-import { render } from "react-dom";
-import { Login } from "../components/Login/Login";
-import styles from "../styles/Home.module.css";
-
-const endPoint1 = "https://join.reckon.com/test1/rangeInfo";
-const endPoint2 = "https://join.reckon.com/test1/divisorInfo";
 
 export default function Home() {
   //create variable to store data from url
-  const [range, setRange] = useState(0);
+
+  const [rangeInfo, setRangeInfo] = useState();
   const [outputDetail, setOutputDetail] = useState([]);
+  //How to use this fomat
+  const [finalOuput, setFinalOutput] = useState({ id: [], value: "" });
   const [fetchError, setFetchError] = useState(false);
 
-  //function for setting if the api success fail or success
-  const apiSuccess = () => {
-    setFetchError(false);
-  };
-  const apiFail = () => {
-    setFetchError(true);
-  };
-
-  //fetching data
-  //TODO: (1) fix the fetching function because sometime its not response properly 
-  const fetchEndPoint = async () => {
-    //use try to prevent from fetch fail api
-    try {
-      const response1 = await fetch(endPoint1);
-      const response2 = await fetch(endPoint2);
-
-      //for the case its not success
-      if (!response1.ok && !response2.ok) {
-        apiFail();
-        throw Error("Did not received data");
-      }
-
-      const handleRes1 = await response1.json();
-      const handleRes2 = await response2.json();
-      //when success set it to the useState variable
-      setRange(handleRes1);
-      setOutputDetail(handleRes2);
-      apiSuccess();
-
-      console.log("range", range);
-      console.log("outputdetail", outputDetail);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-  
-
+  // fetch rangeInfo
   useEffect(() => {
-    // if (fetchError) {
-    //   fetchEndPoint();
-    // }
-    fetchEndPoint();
+    const fetchingRangeInfo = async () => {
+      try {
+        const rangeInfoResponse = await fetch(
+          "https://join.reckon.com/test1/rangeInfo"
+        ); // fetch no cors issue
+        const handleRangeInfo = await rangeInfoResponse.json();
+        if (handleRangeInfo) setRangeInfo(handleRangeInfo);
+      } catch (e) {
+        console.log("e", e);
+      }
+    };
+    fetchingRangeInfo();
   }, []);
 
+  // fetch divisorInfo
+  useEffect(() => {
+    const fetchingDivisorInfo = async () => {
+      try {
+        const divisorInfoResponse = await fetch(
+          "https://join.reckon.com/test1/divisorInfo"
+        ); // fetch no cors issue
+        const handleDivisorInfo = await divisorInfoResponse.json();
+        if (handleDivisorInfo) setOutputDetail(handleDivisorInfo);
+      } catch (e) {
+        console.log("e", e);
+      }
+    };
+    fetchingDivisorInfo();
+  }, []);
+
+  console.log("rangeInfo", rangeInfo);
+  console.log("outputDetail", outputDetail);
+
+  //output component of output detail
+  //TODO: try to access it dynamically
+  const divisorOf3 =
+    outputDetail &&
+    outputDetail.outputDetails &&
+    outputDetail.outputDetails[0] &&
+    outputDetail.outputDetails[0].divisor;
+  const divisorOf5 =
+    outputDetail &&
+    outputDetail.outputDetails &&
+    outputDetail.outputDetails[1] &&
+    outputDetail.outputDetails[1].divisor;
+
+  const outputOf3 =
+    outputDetail &&
+    outputDetail.outputDetails &&
+    outputDetail.outputDetails[0] &&
+    outputDetail.outputDetails[0].output;
+  const outputOf5 =
+    outputDetail &&
+    outputDetail.outputDetails &&
+    outputDetail.outputDetails[1] &&
+    outputDetail.outputDetails[1].output;
+
+  //Check if the fetch success or not
+  if (!outputDetail || !rangeInfo || !outputOf3 || !outputOf5) {
+    return <div>Unable to fetch</div>;
+  }
+
+  const rangeOfNumber = [];
 
   const rendingNumber = () => {
-    //console.log("divisor1", outputDetail.outputDetails[0].divisor);
-    // use these variable just want to see if I call the right object
-    const outDiv3 = outputDetail.outputDetails[0].divisor;
-    const outDiv5 = outputDetail.outputDetails[1].divisor;
-
-    const output3 = outputDetail.outputDetails[0].output;
-    const output5 = outputDetail.outputDetails[1].output;
-
     //the for loop work well for i when tried to console log
     // the problem is that it didnt looping all variable when try to put those statement
-    for (let i = range.lower + 1; i < range.upper + 1; i++) {
-      const listOfRange = i;
-      const divBy3 = listOfRange % outDiv3 === 0;
-      const divBy5 = listOfRange % outDiv5 === 0;
 
-      //TODO: 2) the for loop somehow only loop 1 round of the variable then it stop
-      if (divBy3 && divBy5) {
-        return (
-          <h1>
-            {i} : {output3}
-            {output5}
-          </h1>
-        );
-      }
-      if (divBy3) {
-
-        return (
-          <h1>
-            {i} : {output3}
-          </h1>
-        );
-      }
-      if (divBy5) {
-
-        return (
-          <h1>
-            {i} : {output5}
-          </h1>
-        );
-      } else {
-        return <h1>{i}:</h1>;
-      }
+    for (let i = rangeInfo.lower + 1; i < rangeInfo.upper + 1; i++) {
+      rangeOfNumber.push(i);
     }
   };
+  console.log("rangeOfNumber", rangeOfNumber);
 
-  return <div>{rendingNumber()}</div>;
+  // map function
+  // const blah = [{i: 0, name: 'boss'}, {i: 1, name: ''}, {i: 2, name: 'bosshogg'}]
+  /* blah.map(output => return(<h1>
+            {i} : {outDiv3}
+            {output5}
+          </h1>))  
+*/
+
+  return (
+    <div>
+      <h2>This is test1</h2>
+      {outputDetail.map((value, index) => {
+        const { outputDetails } = value;
+        return <article key="index"></article>;
+      })}
+      {/* if (outcome % divisorOf3 === 0) {
+          return <h1>{outputOf3}</h1>
+        }
+        if (index % divisorOf5 === 0) {
+          name = outputOf5;
+        } else {
+          name = "";
+        }
+        return (
+          <h1 key={outcome}>
+            {outcome}:{name}
+          </h1>
+        ); */}
+
+      {rendingNumber()}
+
+      {/* blah.map(output => return(<h1>
+            {i} : {outDiv3}
+            {output5}
+          </h1>)) */}
+    </div>
+  );
 }
